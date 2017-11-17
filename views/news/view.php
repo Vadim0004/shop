8d6a090387d480cc00339b88fa55fe4a782ee512
user <user@kontent>
2017-11-17 12:55:28 +0200

    Изменил ссылку на <h2>

--- views/news/view.php
+++ views/news/view.php
@@ -38,7 +38,7 @@
 			<div id="page-bgbtm">
 				<div id="content">
 					<div class="post">
-						<h2 class="title"><a href='/news/'><?php echo $newsItem['title'].' # '.$newsItem['id'];?></a></h2>
+						<h2 class="title"><a href='/'><?php echo $newsItem['title'].' # '.$newsItem['id'];?></a></h2>
 						<p class="meta">Posted by <a href="#"><?php echo $newsItem['author_name'];?></a> on <?php echo $newsItem['date'];?>
 							&nbsp;&bull;&nbsp; <a href='/news/' class="permalink"> Back to HomePage</a></p>
 						<div class="entry">
