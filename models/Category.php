feb063dfc67deb2c7732a781f0904cef7b995a6d
Vadim0004 <user@kontent>
2018-01-11 12:43:32 +0200

    Добавил статус для вывода

--- models/Category.php
+++ models/Category.php
@@ -13,13 +13,14 @@ class Category
 
         $categoryList = array();
 
-        $result = $db->query('SELECT id, name FROM category '
+        $result = $db->query('SELECT id, name, status FROM category '
                 . 'ORDER BY sort_order ASC');
 
         $i = 0;
         while ($row = $result->fetch()) {
             $categoryList[$i]['id'] = $row['id'];
             $categoryList[$i]['name'] = $row['name'];
+            $categoryList[$i]['status'] = $row['status'];
             $i++;
         }
 
