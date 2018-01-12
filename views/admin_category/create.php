eba91dc4ddbbec91015e9c4f7a63960d2f36aa87
Vadim0004 <user@kontent>
2018-01-12 12:33:20 +0200

    Добавил файл create.php

---
+++ views/admin_category/create.php
@@ -0,0 +1,68 @@
+<?php include ROOT . '/views/layouts/header_admin.php'; ?>
+
+<section>
+    <div class="container">
+        <div class="row">
+
+            <br/>
+
+            <div class="breadcrumbs">
+                <ol class="breadcrumb">
+                    <li><a href="/admin">Админпанель</a></li>
+                    <li><a href="/admin/category">Управление категориями</a></li>
+                    <li class="active">Добавить категорию</li>
+                </ol>
+            </div>
+
+
+            <h4>Добавить новую категорию</h4>
+
+            <br/>
+
+            <?php if (isset($errors) && is_array($errors)): ?>
+                <ul>
+                    <?php foreach ($errors as $error): ?>
+                        <li> - <?php echo $error; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+
+            <div class="col-lg-4">
+                <div class="login-form">
+                    <form action="#" method="post">
+
+                        <p>Название категории</p>
+                        <input type="text" name="name" placeholder="" value="">
+
+                        <p>Sort_order</p>
+                        <select name="sort_order">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+
+                        <br/><br/>
+
+                        <p>Статус включения и выключения категории</p>
+                        <select name="status">
+                            <option value="1" selected="selected">Включена</option>
+                            <option value="0">Выключена</option>
+                        </select>
+                        
+                        <br/><br/>
+                        
+                        <input type="submit" name="submit" value="Сохранить" class="btn btn-default">
+                        
+                        <br/><br/>
+                        
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php include ROOT . '/views/layouts/footer_admin.php'; ?>
+
