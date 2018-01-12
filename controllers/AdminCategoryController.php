395fb4f242a6648c1ee33d68732477aa5b47011e
Vadim0004 <user@kontent>
2018-01-12 12:32:32 +0200

    Добавил метод actionCreate

--- controllers/AdminCategoryController.php
+++ controllers/AdminCategoryController.php
@@ -15,4 +15,49 @@ class AdminCategoryController extends AdminBase
         return true;
     }
     
+    public function actionCreate()
+    {
+        // провереям пользователя на администратора
+        self::checkAdmin();
+        
+        //Если форма отправлена
+        if (isset($_POST['submit'])) {
+            $option['name'] = $_POST['name'];
+            $option['sort_order'] = $_POST['sort_order'];
+            $option['status'] = $_POST['status'];
+            
+            /*
+            echo '<pre>';
+            print_r($option);
+            echo '</pre>';
+             * 
+             */
+            
+            // Флаг ошибок
+            $errors = false;
+            
+            // Небольшая валидация формы
+            if (!is_array($option) && empty($option)) {
+                $errors[] = 'Ошибка';
+            }
+            
+            if (Category::issetCategory($option['name'])) {
+                $errors[] = 'Есть похожее название в базе данных, используйте другое';
+            }
+            
+            // Если нет ошибок создаем категорию
+            if ($errors == false) {
+                $categoryCreate = Category::createCategory($option);
+                
+                if ($categoryCreate) {
+                    header("Location: /admin/category");
+                }
+                
+            }
+        }
+        
+        require_once ROOT . '/views/admin_category/create.php';
+        return true;
+    }
+    
 }
