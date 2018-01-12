53530c62fb6fc5f02f96a3ab074c608707927ba7
Vadim0004 <user@kontent>
2018-01-12 12:34:12 +0200

    Добавил методы добавления категории и проверки имени категории в БД

--- models/Category.php
+++ models/Category.php
@@ -26,5 +26,49 @@ class Category
 
         return $categoryList;
     }
+    
+    /**
+     * Добавляет запись новой категории в БД
+     * @param array $array <p>Массив параметров из формы</p>
+     * @return boolean
+     */
+    public static function createCategory($array)
+    {
+        $db = Db::getConnection();
+        $sql = 'INSERT INTO category (name, sort_order, status) '
+                . 'VALUES (:name, :sort_order, :status)';
+        $result = $db->prepare($sql);
+        $result->bindParam(':name', $array['name'], PDO::PARAM_STR);
+        $result->bindParam(':sort_order', $array['sort_order'], PDO::PARAM_INT);
+        $result->bindParam(':status', $array['status'], PDO::PARAM_INT);
+
+        if ($result->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+    /**
+     * Проверяет есть ли идентичное название категории в БД
+     * @param type $nameCategory <p>Название категории из формы</p>
+     * @return boolean
+     */
+    public static function issetCategory($nameCategory)
+    {
+       $db = Db::getConnection();
+       $sql = 'SELECT count(*) FROM category '
+               . 'WHERE name = :nameCategory';
+       $result = $db->prepare($sql);
+       $result->bindParam(':nameCategory', $nameCategory, PDO::PARAM_STR);
+       $result->execute();
+       
+       if ($result->fetchColumn()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
