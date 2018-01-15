5d613ebc069782275ca6b66bfba044f4476fe305
Vadim0004 <user@kontent>
2018-01-15 13:22:20 +0200

    Добавил пару строк

--- views/admin_category/delete.php
+++ views/admin_category/delete.php
@@ -1,27 +1,42 @@
-<?php require_once ROOT . '/views/layouts/header_admin.php';?>
+<?php require_once ROOT . '/views/layouts/header_admin.php'; ?>
 <div class="container">
     <div class="row">
-        <?php if (isset($errors) && is_array($errors)):?>
-        <ul>
-            <?php foreach ($errors as $errorsName):?>
-            <li> - <?php echo $errorsName;?></li>
-            <?php endforeach;?>
-        </ul>
-        <?php endif;?>
+
+        <div class="breadcrumbs">
+            <ol class="breadcrumb">
+                <li><a href="/admin">Админпанель</a></li>
+                <li><a href="/admin/category">Управление категориями</a></li>
+                <li class="active">Удалить товар</li>
+            </ol>
+        </div>
         
-        <h4>Удалить категорию # <?php echo $categoryId['id'];?></h4>
+        <?php if (isset($errors) && is_array($errors)): ?>
+            <ul>
+                <?php foreach ($errors as $errorsName): ?>
+                    <li> - <?php echo $errorsName; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+
+        <h4>Удалить категорию # <?php echo $categoryId['id']; ?></h4>
         <br>
-        <h4>Название категории <?php echo $categoryId['name'];?></h4>
+        
+        <h4>Название категории <?php echo $categoryId['name']; ?></h4>
         <br>
+        
         <?php if ($categoryId['status'] == 1): ?>
-        <h4>Статус категории <?php echo 'Включена';?></h4>
-        <?php else:?>
-        <h4>Статус категории <?php echo 'Выключена';?></h4>
-        <?php endif;?>
+            <h4>Статус категории <?php echo 'Включена'; ?></h4>
+        <?php else: ?>
+            <h4>Статус категории <?php echo 'Выключена'; ?></h4>
+        <?php endif; ?>
         </br>
+        
+        <p>Вы действительно хотите удалить этот товар?</p>
+        </br>
+        
         <form action="#" method="POST">
             <input type="submit" name="submit" value="Удалить">
         </form>
     </div>
 </div>
-<?php require_once ROOT . '/views/layouts/footer_admin.php';?>
+<?php require_once ROOT . '/views/layouts/footer_admin.php'; ?>
