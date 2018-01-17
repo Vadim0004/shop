8724fbfc2b113075900f1326c06728e676abaa54
Vadim0004 <user@kontent>
2018-01-17 13:25:24 +0200

    откоректировал таблицы

--- views/admin_order/view.php
+++ views/admin_order/view.php
@@ -10,11 +10,43 @@
                     <li class="active">Просмотр заказа</li>
                 </ol>
             </div>
-
-
-            <h4>Просмотр заказа #<?php echo $order['id']; ?></h4>
-            <br/>
-            
+        <table class="table-bordered table-striped table">
+            <tr>
+                <th>Номер заказа</th>
+                <th>Имя на которое оформлен заказ</th>
+                <th>Телефон пользователя</th>
+                <th>Дата заказа</th>
+                <th>Статус заказа</th>
+                <th>Коментарий пользователя</th>
+            </tr>
+            <tr>
+                <td><?php echo $order['id'];?></td>
+                <td><?php echo $order['user_name'];?></td>
+                <td><?php echo $order['user_phone'];?></td>
+                <td><?php echo $order['date'];?></td>
+                <td><?php Order::getNameStatusOrder($order['status']);?></td>
+                <td><?php echo $order['user_comment'];?></td>
+            </tr>
+        </table>
+        
+        <h4>Список продуктов который купил пользователь</h4>
+        
+        <table class="table-bordered table-striped table">
+            <tr>
+                <th>Название продукта</th>
+                <th>Стоимость продукта</th>
+                <th>Количество продуктов</th>
+                <th>Общая стоимость продуктов</th>
+            </tr>
+                <?php foreach ($product as $productId):?>
+            <tr>
+                <td><?php echo $productId['name'];?></td>
+                <td><?php echo $productId['price'];?></td>
+                <td><?php echo $productQuantity[$productId['id']];?></td>
+                <td><?php echo Product::totalPriceProducts($productQuantity[$productId['id']], $productId['price']);?></td>
+            </tr>
+                <?php endforeach;?>
+        </table>
     </div>
 </div>
 
