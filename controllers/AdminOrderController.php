c22b3e51f91ca0654a484638e2a3e4d1cd9d8a04
Vadim0004 <user@kontent>
2018-01-17 13:22:54 +0200

    + метод actionView

--- controllers/AdminOrderController.php
+++ controllers/AdminOrderController.php
@@ -17,6 +17,20 @@ class AdminOrderController extends AdminBase
     
     public function actionView($id)
     {
+        // провереям пользователя на администратора
+        self::checkAdmin();
+        
+        // Подтягиваем из бд массив с id заказа
+        $order = Order::getOrderById($id);
+        
+        // Получаем из таблицы список продуктов
+        $productQuantity = json_decode($order['products'], true);
+
+        // Получаем массив с индефикаторами товаров
+        $products = array_keys($productQuantity);
+        
+        // Получаем список товаров по id njdfhjd bp vfccbdf
+        $product = Product::getProdustsByIdsArray($products);
         
         require_once ROOT . '/views/admin_order/view.php';
         return true;
