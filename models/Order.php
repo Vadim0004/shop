dd0f1d53daad113b331acd5e4d851d01b11f3107
Vadim0004 <user@kontent>
2018-01-17 13:23:27 +0200

    + getNameStatusOrder

--- models/Order.php
+++ models/Order.php
@@ -77,5 +77,25 @@ class Order
             
             return $arrayOrders;
         }
+        
+    }
+    
+    /**
+     * Возвращает по номеру статуса заказ в каком он состоянии
+     * @param type $name <p>Номер стаутса из БД щрдеров</p>
+     */
+    public static function getNameStatusOrder($name)
+    {
+        switch ($name) {
+            case "1":
+                echo "Новый заказ";
+                break;
+            case "2":
+                echo "В ожидании";
+                break;
+            case "3":
+                echo "Отправлен";
+                break;
+        }
     }
 }
