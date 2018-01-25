5f48f3f5ef2ea1d811646cb21eb08d27fee13ea2
Vadim0004 <user@kontent>
2018-01-25 12:30:28 +0200

    + actionUpdate заказа

--- controllers/AdminOrderController.php
+++ controllers/AdminOrderController.php
@@ -36,4 +36,41 @@ class AdminOrderController extends AdminBase
         return true;
     }
     
+    public function actionUpdate($id)
+    {
+        // Провереям пользователя что он админ
+        self::checkAdmin();
+        
+        //Подтягиваем данные о заказе с БД
+        $ordersItem = Order::getOrderById($id);
+        
+
+        //Если форма отправлена
+        if (isset($_POST['submit'])) {
+            // Записываем данные из формы
+            $option['user_name'] = $_POST['user_name'];
+            $option['user_phone'] = $_POST['user_phone'];
+            $option['user_comment'] = $_POST['user_comment'];
+            $option['status'] = $_POST['status'];
+            
+            //инициализация ошибок
+            $errors = [];
+            
+            //Валидация формы
+            if(!User::checkPhone($option['user_phone'])) {
+                $errors[] = 'Длина телефона должна быть > 10 символов';
+            }
+            // Если ошибок нет отредактирум БД с заказми
+            if ($errors == false) {
+                $result = Order::updateOrder($id, $option);
+                // Перенаправляем на страницу заказы
+                header("Location: /admin/order");
+            }
+            
+        }
+        
+        require_once ROOT . '/views/admin_order/update.php';
+        return true;
+    }
+    
 }
